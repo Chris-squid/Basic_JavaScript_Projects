26ca5f7ba7436448d417ac46ec1e8fa269cefebf
Chris Robinson <oops.ninja@outlook.com>
2020-12-01 12:24:52 -0800

    Update to Main.js 4

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,4 +1,4 @@
-function my_Dictionary() {
+function my_Dictionary() { // Dictionary KVP animal=Cat
     var Animal = {
         Species:"Cat",
         Color:"Black",
@@ -6,6 +6,6 @@ function my_Dictionary() {
         Age:12,
         Sound:"Meow!"
     };
-    delete Animal.Age;
-    document.getElementById("Dictionary").innerHTML = Animal.Sound + Animal.Age + Animal.Breed;
+    delete Animal.Age; // Deleted Age to show Undefined remark
+    document.getElementById("Dictionary").innerHTML = Animal.Sound + Animal.Age + Animal.Breed; // Results shown+ deleted object + extra 
 }
